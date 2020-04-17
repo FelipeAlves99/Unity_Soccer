b8a814920dabe946062022e20ca1c1c6fbefadb4
FelipeAlves99 <falves99.dev@gmail.com>
2020-04-17 00:47:22 -0300

    added force according to the angle

---
+++ SoccerX/Assets/Scripts/Force.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Force : MonoBehaviour
+{
+
+    private Rigidbody2D Ball;
+    private float force = 1000f;
+    private Rotation rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ball = GetComponent<Rigidbody2D>();
+        rotation = GetComponent<Rotation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ApplyForce();
+    }
+
+    private void ApplyForce()
+    {
+        float x = force * Mathf.Cos(rotation.zRotation * Mathf.Deg2Rad);
+        float y = force * Mathf.Sin(rotation.zRotation * Mathf.Deg2Rad);
+
+        if (Input.GetKeyUp(KeyCode.Space))
+            Ball.AddForce(new Vector2(x, y));
+    }
+}
